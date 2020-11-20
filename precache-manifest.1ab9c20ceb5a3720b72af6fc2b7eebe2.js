4f146f20edd8aa0ca6a90575d25326baaf307e96
Kabir Jain <kabirjain15@gmail.com>
2020-11-20 22:20:23 +0530

    Updates

---
+++ precache-manifest.1ab9c20ceb5a3720b72af6fc2b7eebe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95aa42b535fdd4dbe2c32b184c74e77c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9241a7926b0927d69aaa",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "abd70380aa2307e07c32",
+    "url": "/static/js/2.13fea90a.chunk.js"
+  },
+  {
+    "revision": "9241a7926b0927d69aaa",
+    "url": "/static/js/main.7a678340.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0587ebfa17bd9cbf422886ef5ee7d712",
+    "url": "/static/media/profile.0587ebfa.jpg"
+  }
+]);
